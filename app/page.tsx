ede4580df053440e564e5463b8c210e309216f3f
pamyszz <pamelamelllo560@gmail.com>
2024-08-10 19:20:59 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 const Home = () => {
-  return <Button>test</Button>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p>Segunda-feira, 05 de agosto. </p>
+
+        <div className="gap-2 flex items-center mt-6">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className=" rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
